27f32a044ce7d1200546fa1f6784c79e6508edb3
unknown <wangsuidou@163.com>
2016-12-01 23:32:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <my-head></my-head>
+    <my-nav></my-nav>
+  </div>
+</template>
+
+<script>
+import myHead from './components/header'
+import myNav from './components/nav'
+export default {
+  name: 'app',
+  components: {
+    myHead,
+    myNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
