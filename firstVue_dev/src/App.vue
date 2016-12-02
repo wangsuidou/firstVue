3c97f8879f13529c360bb9c5b41e8bd22deb1950
wangshuaijie <hzwsj@tairanchina.com>
2016-12-02 10:39:50 +0800

    ...

---
+++ firstVue_dev/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="#app">
+  <!-- 导航栏 -->
+    <my-head></my-head>
+  </div>
+</template>
+<script>
+import myHead from './components/header'
+export default {
+  name: 'app',
+  components: {
+    myHead
+  }
+}
+</script>
